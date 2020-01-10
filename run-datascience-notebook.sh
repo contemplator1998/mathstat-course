bdcb986db295ee1e0d22ed7ffe6a735407eda4ff
Ivan Zaykov <ivan.zaykov@rozum.com>
2020-01-10 10:30:41 +0300

    add empty notebooks

--- run-datascience-notebook.sh
+++ run-datascience-notebook.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -p 10000:8888 -e JUPYTER_ENABLE_LAB=yes -v "$PWD":/home/jovyan/work jupyter/datascience-notebook
+docker run --rm -p 8888:8888 --privileged -e JUPYTER_ENABLE_LAB=yes -v "$PWD":/home/jovyan/work jupyter/datascience-notebook
